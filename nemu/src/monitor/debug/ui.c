698ef8d0e7f91c39e02cfb86943c1eade25085ac
tracer-ics2019 <tracer@njuics.org>
2020-04-20 05:10:57 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-96-generic #97~16.04.1-Ubuntu SMP Wed Apr 1 03:03:31 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  05:10:57 up 10:17,  1 user,  load average: 0.05, 0.04, 0.10 3bec60faac2b970311b2b8eba67bf027501b66f8

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -196,7 +196,7 @@ static int cmd_p(char *args) {
 static int cmd_x(char *args) {
   int n;
   char arg[256];
-  if (!sscanf(args, "%d %s", &n, arg)) {
+  if (!sscanf(args, "%d %[^\n]", &n, arg)) {
     printf("Pleas enter valid input\n");
     return 0;
   }
