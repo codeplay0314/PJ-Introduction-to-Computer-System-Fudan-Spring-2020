0fd833ba4540f03f1859bbfb48962ed7e0d35367
tracer-ics2019 <tracer@njuics.org>
2020-05-24 09:01:00 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:01:00 up  1:11,  1 user,  load average: 0.45, 1.09, 0.89 d9a06dde71cd3756c102ef4b5512baa5345b6a45

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -4,23 +4,18 @@
 #include "common.h"
 
 typedef struct watchpoint {
-  bool is_free;
   int NO;
-  char msg[100];
-  int val; // value of watch
   struct watchpoint *next;
+  bool is_free, changed;
+  char msg[100];
+  int val, cval;
+}WP;
 
-  /* TODO: Add more members if necessary */
+WP *new_wp(char *, int );
+void free_wp(int);
+void print_wp();
+void print_C_wp();
+void delete_all_wp();
+int check_wp();
 
-
-} WP;
-
-/* TODO: if necessary, try to implement these functions freely in watchpoint.c
- * you can add other functions by yourself if necessary
- */
-
- WP * new_wp(char *, int );
- void free_wp(int);
- void print_wp();
- void delete_all_wp();
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -3,7 +3,7 @@
 
 #define NR_WP 32
 
-static WP wp_pool[NR_WP] = {}; 
+static WP wp_pool[NR_WP] = {};
 
 // TODO: try to re-organize, you can abort head and free_ pointer while just use static int index
 static WP *head, *free_ = NULL;
@@ -13,26 +13,92 @@ void init_wp_pool() {
   for (i = 0; i < NR_WP; i ++) {
     wp_pool[i].NO = i+1;
     wp_pool[i].next = &wp_pool[i + 1];
+    wp_pool[i].is_free = 1;
+    wp_pool[i].changed = wp_pool[i].cval = 0;
   }
   wp_pool[NR_WP - 1].next = NULL;
+
   head = NULL;
   free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
-WP* new_wp(char * msg, int val){
+WP *new_wp(char *msg, int val) {
+  WP* newfree;
+  newfree = (WP*)malloc(sizeof(WP));
+  newfree = free_->next;
+
+  strcpy(free_->msg, msg);
+  free_->val = val; free_->next = head;
+  free_->is_free = 0;free_->changed = 0;free_->cval = 0;
+  head = free_;free_ = newfree;
+
   return free_;
 }
 
-void free_wp(int no){
+void free_wp(int no) {
+  WP* pre_wp;
+  pre_wp = NULL;
+
+  for (WP* now_wp = head;now_wp !=  NULL;now_wp = now_wp->next) {
+    if (now_wp->NO == no) {
+
+      if (pre_wp == NULL) {
+        head = now_wp->next;
+      }
+      else {
+        pre_wp->next = now_wp->next;
+      }
+      now_wp->is_free = 1;
+      now_wp->next = NULL;
+      Log("free watchpoint succeed.");
+      break;
+    }
+    pre_wp = now_wp;
+  }
+
   return;
 }
 
-void print_wp(){
-  printf("watchpoint: not yet implemented");
+void print_wp() {
+  for (WP* now_wp = head;now_wp !=  NULL;now_wp = now_wp->next) {
+    printf("watchpoint %d: msg:%s val:%d\n", now_wp->NO, now_wp->msg, now_wp->val);
+  }
   return;
 }
 
-void delete_all_wp(){
+void print_C_wp() {
+  for (WP* now_wp = head;now_wp !=  NULL;now_wp = now_wp->next) {
+    if (now_wp->changed) {
+      printf("Watchpoint %d: %s\n", now_wp->NO, now_wp->msg);
+      printf("Old value = 0x%08x\n", now_wp->val);
+      printf("New value = 0x%08x\n", now_wp->cval);
+      now_wp->val = now_wp->cval;
+      now_wp->changed = 0;
+    }
+  }
   return;
+}
+
+void delete_all_wp() {
+  for (WP* now_wp = head;now_wp !=  NULL;now_wp = now_wp->next) {
+    free_wp(now_wp->NO);
+  }
+  return;
+}
+
+int check_wp() {
+  int flag = 0;
+  for (WP* now_wp = head;now_wp !=  NULL;now_wp = now_wp->next) {
+    bool *success;
+    success = (bool *)malloc(sizeof(bool));
+    uint32_t now_val = expr(now_wp->msg, success);
+    if (*success) {
+      if (now_wp->val != now_val) {
+        now_wp->changed = 1;now_wp->cval = now_val;
+        flag = 1;
+      }
+    }else {Log("strange expr in check_wp.");}
+  }
+  return flag;
 }
