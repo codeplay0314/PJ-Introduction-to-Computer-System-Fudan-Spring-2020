8f2a9b3fdf55d0e6c0326f631a63501f126ac11b
tracer-ics2019 <tracer@njuics.org>
2020-05-24 00:14:44 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-99-generic #100~16.04.1-Ubuntu SMP Wed Apr 22 23:56:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:14:44 up  4:44,  1 user,  load average: 0.11, 0.16, 0.31 b6890c084990c56724abc2d33955557f3d82f364

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -184,7 +184,7 @@ int eval(int start, int end, bool *success) {
     printf("---%d---\n", mainop);
     int res1 = eval(start, mainop - 1, success), res2 = eval(mainop + 1, end, success);
     if (*success) {
-      printf("[%d %d]\n", start, end);
+      printf("[%d %d] %d %d\n", start, end, res1, res2);
       switch (tokens[mainop].type) {
         case '+': printf("%d\n", res1 + res2); return res1 + res2;
         case '-': printf("%d\n", res1 - res2); return res1 - res2;
