477981e5a9deab39bcb1388bb185d70f479c0170
tracer-ics2019 <tracer@njuics.org>
2020-05-24 00:01:24 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-99-generic #100~16.04.1-Ubuntu SMP Wed Apr 22 23:56:30 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:01:24 up  4:31,  1 user,  load average: 0.52, 0.35, 0.46 c63061c6c9beaa5bf1f5bb4ac80481d4fc18b0ac

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,7 +1,7 @@
 #include "monitor/expr.h" // include expr.h to test expression evaluation program
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
@@ -10,14 +10,14 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
   /* TODO:test your expression evaluation program in here
-   * add your test code to test all expression 
-   * expression are in the file at nemu/tools/gen-expr/input.txt 
+   * add your test code to test all expression
+   * expression are in the file at nemu/tools/gen-expr/input.txt
    */
-  
-  
+
+
   puts("begin to test expr.c\n");
   bool success = true;
-  FILE * fp = NULL;  
+  FILE * fp = NULL;
   char buf[256]={0};
   char content[256] = {0};
   uint32_t ans = 0;
@@ -36,8 +36,8 @@ int main(int argc, char *argv[]) {
       return 1;
     }
   }
-  puts("Expression evaluation testing finished!\n"); 
-  /* Receive commands from user. */ 
+  puts("Expression evaluation testing finished!\n");
+  /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
   return 0;
