63aa2c93eadae8b0d1270fb12f3c5dfd16301755
tracer-ics2019 <tracer@njuics.org>
2020-06-10 00:31:30 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:31:30 up 16:49,  1 user,  load average: 0.54, 0.46, 0.33 547f5c8f41b998538d0d7e43310315df88f03e6c

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -32,7 +32,7 @@ make_group(gp2,
 /* 0xf6, 0xf7 */
 make_group(gp3,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EX(push), EMPTY)
+    EMPTY, EMPTY, EMPTY, EMPTY)
 
 /* 0xfe */
 make_group(gp4,
@@ -42,7 +42,7 @@ make_group(gp4,
 /* 0xff */
 make_group(gp5,
     EMPTY, EMPTY, EMPTY, EMPTY,
-    EMPTY, EMPTY, EMPTY, EMPTY)
+    EMPTY, EMPTY, EX(push), EMPTY)
 
 /* 0x0f 0x01*/
 make_group(gp7,
