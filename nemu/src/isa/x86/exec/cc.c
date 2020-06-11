767821842d857a75a94412df56c5b296064494f4
tracer-ics2019 <tracer@njuics.org>
2020-06-10 22:49:38 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:49:38 up 18:48,  1 user,  load average: 0.67, 0.28, 0.15 4bbccc0f687b9474c011749210cb5f572152d57

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -13,18 +13,36 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
-  switch (subcode & 0xe) {
+    switch (subcode & 0xe) {
     case CC_O:
+      rtl_get_OF(dest);
+      break;
     case CC_B:
+      rtl_get_CF(dest);
+      break;
     case CC_E:
+      rtl_get_ZF(dest);
+      break;
     case CC_BE:
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1);
+      break;
     case CC_S:
+      rtl_get_SF(dest);
+      break;
     case CC_L:
-    case CC_LE:
-      TODO();
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_or(dest, &t0, &t1);
+      break;
+    case CC_LE:{
+      rtl_li(dest, ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF)));
+      break;
+    }
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
-  }
+    }
 
   if (invert) {
     rtl_xori(dest, dest, 0x1);
