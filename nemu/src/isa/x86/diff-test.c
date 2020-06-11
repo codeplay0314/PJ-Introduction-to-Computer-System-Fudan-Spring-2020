7c7a34ca22d0b954a9d479d9c59b423d245a1cb0
tracer-ics2019 <tracer@njuics.org>
2020-06-11 06:36:52 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  06:36:52 up 1 day,  2:35,  1 user,  load average: 0.87, 0.54, 0.26 855d2dd5f318d068ea16fb60e76517563ac38d96

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,11 +2,14 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  for (int i = 0 ; i < 8 ; i++)
+  for (int i = 0 ; i < 8 ; i++) {
     if (reg_l(i) != ref_r->gpr[i]._32) {
-      printf("[reg %d]\tnemu: 0x%x\tqemu: 0x%x\n", i, reg_l(i), (uint32_t)ref_r->gpr[i]._32);
+      printf("[reg %d]\tnemu: 0x%x\tqemu: 0x%x\n", i, reg_l(i), ref_r->gpr[i]._32);
       return false;
     }
+    printf("0x%x ", ref_r->gpr[i]._32);
+  }
+  putchar('\n');
   return true;
 }
 
