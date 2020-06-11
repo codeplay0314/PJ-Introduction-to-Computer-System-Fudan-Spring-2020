36c60fd51daa88e6b49cb4143e3a2b362224d4e9
tracer-ics2019 <tracer@njuics.org>
2020-06-10 22:56:45 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:56:45 up 18:55,  1 user,  load average: 0.08, 0.18, 0.14 2b42000167d68067a5eb65d43f49aac4617b5b39

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -2,7 +2,11 @@
 #include "cc.h"
 
 make_EHelper(test) {
-  TODO();
+  rtl_and(&s0, &id_dest->val, &id_src->val);
+  s1 = 1;
+  rtl_set_CF(&s1);
+  rtl_set_OF(&s1);
+  rtl_update_ZFSF(&s0, id_dest->width);
 
   print_asm_template2(test);
 }
