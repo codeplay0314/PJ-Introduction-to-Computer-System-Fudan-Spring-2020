90aec114ccabc190b9a5d7c51becd8c230811e60
tracer-ics2019 <tracer@njuics.org>
2020-06-11 04:34:43 -0700

    >  compile 18307130163 codeplay0314 Linux ubuntu 4.15.0-101-generic #102~16.04.1-Ubuntu SMP Mon May 11 11:38:16 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  04:34:43 up 1 day, 33 min,  1 user,  load average: 0.58, 0.26, 0.14 27c05e2dbcc3aaff94d9d7f9f33cbdffab2697d3

--- nemu/src/isa/x86/include/isa/reg.h
+++ nemu/src/isa/x86/include/isa/reg.h
@@ -31,25 +31,23 @@ typedef struct{
     struct {
       rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
     };
-
-    union {
-        struct {
-            uint32_t CF: 1;
-            unsigned: 5;
-            uint32_t ZF: 1;
-            uint32_t SF: 1;
-            unsigned: 1;
-            uint32_t IF: 1;
-            unsigned: 1;
-            uint32_t OF: 1;
-            unsigned: 20;
-        };
-        rtlreg_t val;
-      }eflags;
-
   };
-
   vaddr_t pc;
+
+  union {
+    rtlreg_t val;
+    struct {
+      uint32_t CF: 1;
+      unsigned: 5;
+      uint32_t ZF: 1;
+      uint32_t SF: 1;
+      unsigned: 1;
+      uint32_t IF: 1;
+      unsigned: 1;
+      uint32_t OF: 1;
+      unsigned: 20;
+    };
+  }eflags;
 
 } CPU_state;
 
